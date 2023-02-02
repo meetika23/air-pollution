4b7aedec5af11d14ba201ea6efaead686065bcd8
meetika23 <mitikajain2001@gmail.com>
2023-02-02 23:02:49 +0530

    Flask App completed

--- app.py
+++ app.py
@@ -10,20 +10,49 @@ def home():
 @app.route('/linearregression', methods = ['GET','POST'])
 def linear_regression():
     if request.method == 'POST':
-        LR_model = pickle.load(open('LR_model.pkl','rb'))
+        model = pickle.load(open('LR_model.pkl','rb'))
         features = [int(x) for x in request.form.values()]
-        final_values = [np.array(features)]
-        prediction = LR_model.predict(final_values)
-        # print(prediction)
-        output = '{0:.{1}f}'.format(prediction[0],2)
-        return render_template("linearregression.html", pred =  'Predicted AQI is {}'.format(output) )
+        return generate_output(model,features,"linearregression")
     else:
         return render_template("linearregression.html")
 
-   
-    
-    
-    
+@app.route('/logisticregression', methods = ['GET','POST'])
+def logistic_regression():
+    if request.method == 'POST':
+        model = pickle.load(open('LogR_model.pkl','rb'))
+        features = [int(x) for x in request.form.values()]
+        return generate_output(model,features,"logisticregression")
+    else:
+        return render_template("logisticregression.html")
+
+@app.route('/randomforest', methods = ['GET','POST'])
+def randomforest():
+    if request.method == 'POST':
+        model = pickle.load(open('RandF_model.pkl','rb'))
+        features = [int(x) for x in request.form.values()]
+        return generate_output(model,features,"randomforest")
+    else:
+        return render_template("randomforest.html")
+
+@app.route('/decisiontree', methods = ['GET','POST'])
+def decisiontree():
+    if request.method == 'POST':
+        model = pickle.load(open('DecT_model.pkl','rb'))
+        features = [int(x) for x in request.form.values()]
+        return generate_output(model,features,"decisiontree")
+    else:
+        return render_template("decisionTree.html")
+
+def generate_output(model,features,str):
+    final_values = [np.array(features)]
+    prediction = model.predict(final_values)
+    # print(prediction)
+    if str == "linearregression":
+        output = '{0:.{1}f}'.format(prediction[0],2)
+        return render_template("{}.html".format(str), pred =  'Predicted AQI is {}'.format(output) )
+    else:
+        return render_template("{}.html".format(str), pred =  'Air Quality is {}'.format(prediction[0]) )
+
 
 if __name__ == "__main__":
     app.run(debug = True)
